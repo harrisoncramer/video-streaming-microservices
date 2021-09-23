9953a54d8dad8f04e3094d5364355de706380cb5
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-22 22:26:39 -0400

    Refactored video-streaming microservice.
    Using .then chaining instead of IFFE async/await.

--- video-streaming/index.js
+++ video-streaming/index.js
@@ -17,55 +17,72 @@ const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 
-(async () => {
-  /* Connect to MongoDB */
-  let db;
-  try {
-    const client = await MongoClient.connect(DB_HOST);
+MongoClient.connect(DB_HOST)
+  .then((client) => {
     db = client.db(DB_NAME);
-  } catch (err) {
-    console.error('Could not connect to DB.');
-    throw err;
-  }
+    const videos = db.collection('videos');
 
-  /* Create videos collection */
-  const videos = db.collection('videos');
+    ///////////////////////
+    // LISTEN FOR VIDEOS //
+    ///////////////////////
 
-  app.get('/video', async (req, res) => {
-    let record;
-    console.log(`Searching for ${req.query.id}`);
-    try {
-      record = await videos.findOne({ _id: new ObjectId(req.query.id) });
-    } catch (err) {
-      console.error('Database query failed.');
-      console.error(err);
-      res.sendStatus(500);
-      return;
-    }
-
-    if (!record) {
-      res.sendStatus(404);
-      return;
-    }
-
-    const forwardRequest = http.request(
-      {
-        host: VIDEO_STORAGE_HOST,
-        port: VIDEO_STORAGE_PORT,
-        path: `/video?path=${record.videoPath}`,
-        method: 'GET',
-        headers: req.headers,
-      },
-      (forwardResponse) => {
-        res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-        forwardResponse.pipe(res);
+    app.get('/video', async (req, res) => {
+      let record;
+      console.log(`Searching for ${req.query.id}`);
+      try {
+        record = await videos.findOne({ _id: new ObjectId(req.query.id) });
+      } catch (err) {
+        console.error('Database query failed.');
+        console.error(err);
+        res.sendStatus(500);
+        return;
       }
-    );
 
-    req.pipe(forwardRequest);
-  });
+      if (!record) {
+        res.sendStatus(404);
+        return;
+      }
 
-  app.listen(PORT, () => {
-    console.log(`Video streaming microservice listening!`);
+      const forwardRequest = http.request(
+        {
+          host: VIDEO_STORAGE_HOST,
+          port: VIDEO_STORAGE_PORT,
+          path: `/video?path=${record.videoPath}`,
+          method: 'GET',
+          headers: req.headers,
+        },
+        (forwardResponse) => {
+          res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+          forwardResponse.pipe(res);
+        }
+      );
+
+      req.pipe(forwardRequest);
+    });
+
+    //////////////////////////
+    // GLOBAL ERROR HANDLER //
+    //////////////////////////
+
+    const defaultErrorObject = {
+      status: 500,
+      log: 'An uncaught error occured on the server.',
+    };
+
+    app.use((err, _req, res, _next) => {
+      const errorObject = { ...defaultErrorObject, ...err };
+      res.status(errorObject.status).send(errorObject.log);
+    });
+
+    ////////////
+    // LISTEN //
+    ////////////
+
+    app.listen(PORT, () => {
+      console.log(`Video streaming microservice listening!`);
+    });
+  })
+  .catch((err) => {
+    console.error('Could not connect to DB.');
+    console.error(err);
   });
-})();
