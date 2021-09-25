2e427bd59cfb0cfade4d4de42cac9754d82bb2da
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-25 12:19:11 -0400

    Added rabbitmq to history microservice
    Reconfigured docker-compose to wait for connection.

--- history/src/index.js
+++ history/src/index.js
@@ -1,41 +1,42 @@
 const express = require('express');
 const http = require('http');
+const amqplib = require('amqplib');
 const { MongoClient, ObjectId } = require('mongodb');
 
-const { DB_HOST, DB_NAME, PORT } = process.env;
-if (!DB_HOST || !DB_NAME || !PORT) {
+const { DB_HOST, DB_NAME, PORT, RABBIT } = process.env;
+if (!DB_HOST || !DB_NAME || !PORT || !RABBIT) {
   throw new Error('Please set environment variables!');
 }
 
-function startHttpServer(db) {
-  return new Promise((resolve) => {
-    const app = express();
-    app.use(express.json());
-    const videosCollection = db.collection('video-history');
-
-    app.post('/viewed', (req, res) => {
-      const { videoPath } = req.body;
-      videosCollection
-        .insertOne({ videoPath })
-        .then(() => {
-          console.log(`Added video ${videoPath} to history.`);
-          res.sendStatus(200);
-        })
-        .catch((err) => {
-          console.error(`Error adding video ${videoPath} to history.`);
-          console.error(err);
-          res.sendStatus(500);
-        });
-    });
-
-    app.listen(parseInt(PORT), () => {
-      resolve();
-    });
-  });
+/* Connect to RabbitMQ and create a messaging channel */
+async function connectRabbit() {
+  const messagingConnection = await amqplib.connect(RABBIT);
+  return messagingConnection.createChannel();
 }
 
+/* Establish DB connection */
 MongoClient.connect(DB_HOST).then((client) => {
   db = client.db(DB_NAME);
+  const videosCollection = db.collection('video-history');
 
-  startHttpServer(db).then(() => console.log('History microservice online.'));
+  /* Setup express application */
+  const app = express();
+  app.use(express.json());
+
+  app.post('/viewed', async (req, res) => {
+    const { videoPath } = req.body;
+    try {
+      await videosCollection.insertOne({ videoPath });
+      console.log(`Added video ${videoPath} to history.`);
+      res.sendStatus(200);
+    } catch (err) {
+      console.error(`Error adding video ${videoPath} to history.`);
+      console.error(err);
+      res.sendStatus(500);
+    }
+  });
+
+  app.listen(parseInt(PORT), () => {
+    console.log('History microservice online.');
+  });
 });
