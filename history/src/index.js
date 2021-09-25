344f45269b4bdd7cb7d9b346564a12544e336616
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-25 12:49:06 -0400

    Setup history code with async/await.

--- history/src/index.js
+++ history/src/index.js
@@ -8,18 +8,22 @@ if (!DB_HOST || !DB_NAME || !PORT || !RABBIT) {
   throw new Error('Please set environment variables!');
 }
 
+/* Establish DB connection */
+async function connectToMongoDB(collectionName) {
+  const client = await MongoClient.connect(DB_HOST);
+  db = client.db(DB_NAME);
+  const collection = db.collection(collectionName);
+  return collection;
+}
+
 /* Connect to RabbitMQ and create a messaging channel */
 async function connectRabbit() {
   const messagingConnection = await amqplib.connect(RABBIT);
   return messagingConnection.createChannel();
 }
 
-/* Establish DB connection */
-MongoClient.connect(DB_HOST).then((client) => {
-  db = client.db(DB_NAME);
-  const videosCollection = db.collection('video-history');
-
-  /* Setup express application */
+/* Setup express application */
+function server(videosCollection) {
   const app = express();
   app.use(express.json());
 
@@ -39,4 +43,47 @@ MongoClient.connect(DB_HOST).then((client) => {
   app.listen(parseInt(PORT), () => {
     console.log('History microservice online.');
   });
+}
+
+async function main() {
+  let videosCollection;
+  try {
+    videosCollection = await connectToMongoDB('video-history');
+  } catch (err) {
+    console.error('Could not connect to MongoDB.');
+    throw err;
+  }
+
+  /* Callback for consuming RabbitMQ messages */
+  async function consumeViewedMessage(msg) {
+    const parsedMessage = JSON.parse(msg.content.toString());
+    try {
+      await videosCollection.insertOne({ videoPath: parsedMessage.videoPath });
+    } catch (err) {
+      console.error('Could not insert message into DB.');
+      throw err;
+    }
+  }
+
+  let messageChannel;
+  try {
+    messageChannel = await connectRabbit();
+    await messageChannel.assertQueue('viewed', {});
+    messageChannel.consume('viewed', consumeViewedMessage);
+  } catch (err) {
+    console.error('Could not connect to RabbitMQ');
+    throw err;
+  }
+
+  server(videosCollection);
+}
+
+/*
+ * This function connects to MongoDB, connects to RabbitMQ, and
+ * starts up our Express server. When we recieve a request to /viewed on
+ * our Express server, we insert
+ * */
+main().catch((err) => {
+  console.error('An error occured.');
+  console.error(err);
 });
